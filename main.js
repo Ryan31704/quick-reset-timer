b9bda6dcdb8425f71d8760d8d47f9f4f124010e0
Ryan Alli <ryanalli77@gmail.com>
2023-12-30 00:36:54 -0500

    resizing for mobile devices

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let startTime = 0;
+let currTime = 0;
 
 document.getElementById("start-button").onclick = function()
 {
   startTime = Number(document.getElementById("text-box").value);
   document.getElementById("clock-text").innerHTML = startTime;
+  currTime = startTime;
   console.log(startTime);
 }
