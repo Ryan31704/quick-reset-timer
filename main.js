a78187616a8c2760e458c0b2497fc98a7ad04dd7
Ryan Alli <ryanalli77@gmail.com>
2023-12-30 13:12:53 -0500

    added countdown functionality

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 let startTime = 0;
-let currTime = 0;
+let now = 0;
+let countDownDate = 0;
 
-document.getElementById("start-button").onclick = function()
+document.getElementById("reset-button").onclick = function()
 {
+  now = new Date().getTime();
+  //grabs from text box on start
   startTime = Number(document.getElementById("text-box").value);
+  //changes text on page
   document.getElementById("clock-text").innerHTML = startTime;
-  currTime = startTime;
-  console.log(startTime);
+  //calculates date to count to
+  countDownDate = new Date().getTime() + (1000 * startTime) + 1000
+  countDown();
+}
+function countDown()
+{
+  setInterval(() => {
+
+    now = new Date().getTime();
+    timeleft = countDownDate - now;
+    let seconds = Math.floor(timeleft / 1000);
+    document.getElementById("clock-text").innerHTML = seconds;
+    console.log(seconds);
+    if(timeleft < 0)
+    {
+      clearInterval();
+      document.getElementById("clock-text").innerHTML = 0;
+    }
+}, 1000);
 }
